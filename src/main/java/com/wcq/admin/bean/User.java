e90cd86567015d49212855acd1ca2abe3a9496bf
wucanqin <1611743891@qq.com>
2022-11-24 17:17:21 +0800

    test

--- src/main/java/com/wcq/admin/bean/User.java
+++ src/main/java/com/wcq/admin/bean/User.java
@@ -1,10 +1,18 @@
 package com.wcq.admin.bean;
 
+import com.sun.xml.internal.ws.developer.Serialization;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-public class User {
+@AllArgsConstructor
+@NoArgsConstructor
 
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String username;
     private String password;
 
--- src/main/java/com/wcq/admin/controller/IndexController.java
+++ src/main/java/com/wcq/admin/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
 @Slf4j
 @Controller
